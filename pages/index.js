43c82d6a46405263ba0af1f08b001640b09e501d
marcapaginas <40990440+marcapaginas@users.noreply.github.com>
2020-12-05 20:51:58 +0100

    cambio titulo

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home({ isConnected }) {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
+          Welcome to <a href="https://nextjs.org">Next.js with MongoDB & Tailwind!</a>
         </h1>
 
         {isConnected ? (
